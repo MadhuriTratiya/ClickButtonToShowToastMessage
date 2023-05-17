692d8e2894b9a0ffe55fcde9226a7787e3fe6254
madhuri <madhuritratiya@gmail.com>
2023-05-17 23:14:41 +0530

    Initial commit

---
+++ app/src/main/java/madhuri/com/clickme/MainActivity.java
@@ -0,0 +1,23 @@
+package madhuri.com.clickme;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Button button = (Button) findViewById(R.id.btn);
+    }
+
+
+    public void clickmybutton(View view) {
+        Toast.makeText(this, "Welcome", Toast.LENGTH_LONG).show();
+    }
+}
